99b8b1baffa56d01e47f284ce04cf6d212b2e507
Robert Rinaldo <rinaldo646@gmail.com>
2018-07-03 00:37:40 -0400

    inputs of type number are now converted from strings to numbers

--- app/components/GameCreationModal.jsx
+++ app/components/GameCreationModal.jsx
@@ -20,7 +20,9 @@ class QuestionInput extends Component {
     this.state = initialState
   }
 
-  handleChange(_, { name, value, checked }) {
+  handleChange(_, { name, value, checked, type }) {
+    if (value === undefined) value = checked
+    else if (value && type === 'number') value = +value
     this.setState({ [name]: value === undefined ? checked : value })
   }
 
@@ -36,6 +38,7 @@ class QuestionInput extends Component {
           <Form>
             <Form.Input
               name="title"
+              type="text"
               label="Title"
               placeholder="Enter a title"
               value={this.state.title}
@@ -45,6 +48,7 @@ class QuestionInput extends Component {
               autoHeight
               rows={2}
               name="description"
+              type="text"
               label="Description"
               placeholder="Enter a description"
               value={this.state.description}
@@ -53,18 +57,21 @@ class QuestionInput extends Component {
             <Form.Group widths="equal">
               <Form.Input
                 name="width"
+                type="number"
                 label="Number of Categories"
                 value={this.state.width}
                 onChange={this.handleChange}
               />
               <Form.Input
                 name="height"
+                type="number"
                 label="Questions per Category"
                 value={this.state.height}
                 onChange={this.handleChange}
               />
               <Form.Input
                 name="multiplier"
+                type="number"
                 label="Base Points"
                 value={this.state.multiplier}
                 onChange={this.handleChange}
