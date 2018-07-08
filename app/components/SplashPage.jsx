70263799207bca65fcc0f82d513e00998d03812d
Robert Rinaldo <rinaldo646@gmail.com>
2018-07-08 15:29:08 -0400

    added minimal content to splash page

--- app/components/SplashPage.jsx
+++ app/components/SplashPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const SplashPage = () => (
-  <div>
+  <div style={{ padding: '3em', textAlign: 'center' }}>
     <h2>Welcome to Peril</h2>
-    <p>This page is a work in progress. Please log in to continue</p>
+    <h3>A website for browsing, creating, and hosting Jeopardy-like games</h3>
+    <p>This website is a work in progress. It is not fully styled and features are subject to change.</p>
+    <p>Please sign up or log in to continue.</p>
   </div>
 )
 
