5f62c10b26cc991acab22e5bfcfd3a25a66ec37f
Robert Rinaldo <rinaldo646@gmail.com>
2018-07-09 23:38:22 -0400

    added deploy script

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const merge = require('webpack-merge')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const common = require('./webpack.common.js')
 
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   devtool: 'source-map',
   mode: 'production',
   plugins: [
-    new BundleAnalyzerPlugin(),
+    // new BundleAnalyzerPlugin(),
     new UglifyJSPlugin({
       sourceMap: true
     }),
